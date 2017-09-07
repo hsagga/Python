a8e11d8c42a2ef1768d0023ea688a3092817a7bb
hsagga <hsagga@calstatela.edu>
2017-09-07 15:45:08 -0700

    one file added

---
+++ AcmDemo.py
@@ -0,0 +1,28 @@
+'''
+Created on Sep 7, 2017
+
+@author: lil_h
+'''
+# if statement
+x = 15
+y = 10    
+if x > y:
+    print('wassup') 
+    
+    
+# for statement
+for z in range(0,10,1):
+    print(z)  
+    
+# List and array
+
+myList = [1, 2, 3, 4, 5]
+myList1 = ['beacon', 4, 4.5]
+myList.insert(2, 'football')
+myList.append("google")
+myList.reverse()
+for x in myList:
+    print(x)       
+    
+    
+#     
